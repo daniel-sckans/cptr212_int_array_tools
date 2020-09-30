7569d6bfa50fc250b59afce9a5d5b1098c9b2242
Ashley-Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2020-09-30 15:46:21 -0500

    Add files via upload

---
+++ arr_rev.c
@@ -0,0 +1,8 @@
+#include "iat.h"
+
+int arr_rev(int* arr, unsigned int arr_len){ //might need to be arr_len
+    for(int i = 0; i < arr_len; i++){
+        //Reverse the order of the array
+    }
+    return 0;
+}
