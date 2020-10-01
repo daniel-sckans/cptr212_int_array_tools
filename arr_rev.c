eec42d41e416a9983ffaf604452d7ca9ff10e2fb
Ashley-Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2020-10-01 15:51:08 -0500

    Update arr_rev.c

--- arr_rev.c
+++ arr_rev.c
@@ -1,16 +1,16 @@
 #include "iat.h"
 
 int arr_rev(int* arr, unsigned int arr_len){
-    int *begin = &arr[0];
-    int *end = &arr[arr_len - 1];
+    int *begin = &arr[0]; //first address of first value in array
+    int *end = &arr[arr_len - 1]; //address of last value in array
     
-    while (begin != end && begin < end){
-        *begin ^= *end;
+    while (begin != end && begin < end){ //while value of begin and end are not the same and begin is less than end
+        *begin ^= *end; //Use XOR to flip the values
         *end ^= *begin;
         *begin ^= *end;
 
-        begin++;
-        end--;
+        begin++; //move inward from the right by one
+        end--; //move inward from the left by one
     }
     return 0; 
 }
